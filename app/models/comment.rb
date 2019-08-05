e73bb50c63064fa8715501a0fe107ff0f4aae55e
Tony Baker <this.tonybaker@gmail.com>
2019-08-05 18:16:29 +0000

    Done.

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -3,6 +3,7 @@ class Comment < ActiveRecord::Base
   belongs_to :post
   accepts_nested_attributes_for :user
 
-def user_attributes=(user_attributes)
-  self.user = User.find_or_create_by(user_attributes)
+  def user_attributes=(user_attributes)
+    self.user = User.find_or_create_by(username: user_attributes[:username])
+  end
 end
